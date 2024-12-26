b99939e75da3b3a4207fbc423ba273cbf7905ae1
Adrian5519 <luisadrian_cabrera@hotmail.com>
2024-12-26 15:44:19 -0800

    Remove_Element_1

---
+++ Remove_Element/Remove_Element_1.cpp
@@ -0,0 +1,23 @@
+// Solution 1 | Original
+
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int removeElement(std::vector<int>& nums, int val) {
+
+        int k = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (nums[i] != val) {
+                nums[k] = nums [i];
+                k++;
+            }
+        }
+
+        return k;
+
+    }
+};
