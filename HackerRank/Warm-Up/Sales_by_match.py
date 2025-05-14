0af45d975cfba47479cd35cd10b7b8c4b9c91e0e
LuisAdrian5519 <luisadrian.cabrera5519@gmail.com>
2025-05-14 13:25:58 -0700

    Hck_Sales

--- HackerRank/Warm-Up/Sales_by_match.py
+++ HackerRank/Warm-Up/Sales_by_match.py
@@ -4,15 +4,6 @@ import random
 import re
 import sys
 
-#
-# Complete the 'sockMerchant' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER_ARRAY ar
-#
-
 def sockMerchant(n, ar):
     
     # Generate a Hash with values and frecuencies
